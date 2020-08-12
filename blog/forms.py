5902e0e48c8d5b84d8765df0ce8882ed3a429c79
vinay kollu <kolluvinay425@gmail.com>
2020-08-12 22:43:34 +0530

    first commit

---
+++ blog/forms.py
@@ -0,0 +1,20 @@
+from .models import Comment,Post
+from django import forms
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields =('title','content')
+        widgets = {
+            
+           
+            'content': SummernoteWidget(),
+        }
